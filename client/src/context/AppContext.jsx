1ff2ce9cddd9a2776ebcd7886ded6bd1204a3386
lokeshkonka <konkalokesh372@gmail.com>
2025-08-24 10:58:25 +0530

    Client context

---
+++ client/src/context/AppContext.jsx
@@ -0,0 +1,18 @@
+/* eslint-disable react-refresh/only-export-components */
+import { createContext, useContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+export const   AppContext = createContext();
+
+export const AppContextProvider = ({children})=>{
+    const navigate = useNavigate();
+    const [user,setUser]= useState(null)
+    const [isSeller,setSeller]= useState(false)
+    const value={navigate,user,setSeller,setUser,isSeller}
+    return<AppContext.Provider  value={value}>
+        {children}
+    </AppContext.Provider>
+}
+export const useAppContext = ()=>{
+    return useContext(AppContext)
+}
