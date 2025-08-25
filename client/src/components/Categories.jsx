6c12a8180b69089e8cc4a0492a71392138717678
lokeshkonka <konkalokesh372@gmail.com>
2025-08-25 19:26:05 +0530

    Done With Banner

---
+++ client/src/components/Categories.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { assets } from '../assets/assets'
+
+const Categories = () => {
+  return (
+  <>
+  <p>Categories</p>
+  <div>
+    <div>
+        <img src={assets.box_icon} alt="" />
+        <p>fruit</p>
+    </div>
+  </div>
+  </>
+  )
+}
+
+export default Categories
