61f102b906b57f1826eb63b4996fcf853afdc2e3
lokeshkonka <konkalokesh372@gmail.com>
2025-09-04 13:07:05 +0530

    Address Page added

--- client/src/pages/AddAddress.jsx
+++ client/src/pages/AddAddress.jsx
@@ -3,7 +3,7 @@ import { assets } from '../assets/assets'
 //input field
 const Inputfield = ({type,placeholder,name,handleChange,address})=>(
     <input
-    className='w-full py-2.5 border border-gray-500/30 rounded outline-none text-gray focus:border-primary transition'
+    className='w-full py-3.5 border border-gray-500/30 rounded outline-none text-gray focus:border-primary transition'
     type={type}
     placeholder={placeholder}
     onChange={handleChange}
@@ -16,13 +16,30 @@ const Inputfield = ({type,placeholder,name,handleChange,address})=>(
 
 const AddAddress = () => {
 
-    const [address,setAddress]  = useState({})
+    const [address,setAddress]  = useState({
+        firstName : '',
+        lastName : '',
+        email : '',
+        street : '',
+        city : '',
+        state : '',
+        zipcode : '',
+        country : '',
+        phone : '',
+    })
+
     const OnSubmitHandler = async (e)=>{
     e.preventDefault() // stops the webpage from reloading
 }
-const handleChange = async (e)=>{
-    e.preventDefault() // stops the webpage from reloading
+const handleChange =  (e)=>{
+   const {name,value}= e.target; // stops the webpage from reloading
+   setAddress((prevAddress)=>({
+    ...prevAddress,
+    [name]:value
+   }))
+
 }
+
   return (
     <div className='mt-16 pb-16'>
         <p
@@ -39,10 +56,27 @@ const handleChange = async (e)=>{
                 <form onSubmit={OnSubmitHandler}
                 className='space-y-3 mt-6 text-sm'
                 >
-                    <div>
+                    <div className='grid grid-cols-2 gap-4' >
                         <Inputfield handleChange={handleChange} address={address} name="firstname" type="text" placeholder={"First Name"}/>
                         <Inputfield handleChange={handleChange} address={address} name="lastname" type="text" placeholder={"Last Name"}/>
                     </div>
+                        <Inputfield handleChange={handleChange} address={address} name="email" type="email" placeholder={"Email Address"}/>
+                        <Inputfield handleChange={handleChange} address={address} name="street" type="text" placeholder={"Street"}/>
+                    <div className='grid grid-cols-2 gap-4'>
+                        <Inputfield handleChange={handleChange} address={address} name="city" type="text" placeholder={"City"}/>
+                        <Inputfield handleChange={handleChange} address={address} name="state" type="text" placeholder={"State"}/>                                                                                          
+                    </div>
+                    <div className='grid grid-cols-2 gap-4'>
+                        <Inputfield handleChange={handleChange} address={address} name="zipcode" type="text" placeholder={"Zipcode"}/>
+                        <Inputfield handleChange={handleChange} address={address} name="country" type="text" placeholder={"Country"}/>                                                                                          
+                    </div>
+                        <Inputfield handleChange={handleChange} address={address} name="phone" type="text" placeholder={"Phone"}/>                                                                                          
+                        <button
+                        className='w-full mt-6 bg-primary text-white py-3 hover:bg-primary-dull transition cursor-pointer uppercase'
+                        onClick={()=>{}}
+                        >
+                            Save Address
+                        </button>
                 </form>
             </div>
             <img
